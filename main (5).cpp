ba75e108e256bae74f9d61c1c173625f75075f12
Sneha659 <95531823+Sneha659@users.noreply.github.com>
2021-12-26 12:53:27 +0530

    Add files via upload

---
+++ main (5).cpp
@@ -0,0 +1,20 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, PHP, Ruby, 
+C#, VB, Perl, Swift, Prolog, Javascript, Pascal, HTML, CSS, JS
+Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+#include <iostream>
+using namespace std;
+int main()
+{
+    int n;
+    cout<<"enter the number"<<endl;
+    cin>>n;
+    cout<<"table of"<<n<<endl;
+    for(int i=1;i<=10;i++)
+    cout<<n*i<<endl;
+    return 0;
+}
