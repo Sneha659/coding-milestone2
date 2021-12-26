df0674d51a2a61a551398ed854f8f99585abe001
Sneha659 <95531823+Sneha659@users.noreply.github.com>
2021-12-26 12:52:09 +0530

    Add files via upload

---
+++ main (2).cpp
@@ -0,0 +1,21 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, PHP, Ruby, 
+C#, VB, Perl, Swift, Prolog, Javascript, Pascal, HTML, CSS, JS
+Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+#include <iostream>
+using namespace std;
+int main()
+{
+    int l,b;
+    cout<<"enter the length and breadth"<<endl;
+    cin>>l>>b;
+    if(l==b)
+    {cout<<"it is square"<<endl;}
+    else
+    {cout<<"it is not a square"<<endl;}
+    return 0;
+}
